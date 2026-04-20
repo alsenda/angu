a631b6a82a82907d950e68a5878fe9fe0fb93d14
Álvaro Sendón Aleu <alvaro.sendon.aleu@gmail.com>
2026-04-20 15:30:52 +0200

    fix: remove unused Link import in CoursesPage

--- client/src/pages/CoursesPage.tsx
+++ client/src/pages/CoursesPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { api } from '../lib/api';
 import { useAuthStore } from '../store/authStore';
 import Navbar from '../components/layout/Navbar';
